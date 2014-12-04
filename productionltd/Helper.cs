3e728621f9231da7a4a71c2c509ad3fe1b7052a9
nicoeg <nicomanden@gmail.com>
2014-12-04 11:32:10 +0100

    Added Helper class

---
+++ productionltd/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productionltd {
+    static class Helper {
+
+        public DateTime GetNextWeekday(DateTime start, DayOfWeek day) {
+            // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+            int daysToAdd = ((int)day - (int)start.DayOfWeek + 7) % 7;
+            return start.AddDays(daysToAdd);
+        }
+    }
+}
