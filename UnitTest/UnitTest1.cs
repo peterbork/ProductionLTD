b7288a7a91a8c4bd5eaf4712d39d23fe3e3b75d4
peterbork <peter-b-n@hotmail.com>
2014-12-08 08:59:13 +0100

    Unit test added

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Order;
+
+namespace UnitTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+
+        }
+    }
+}
