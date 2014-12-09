a48401673e4dc390a5639d9a9b4310719aa6fe99
peterbork <peter-b-n@hotmail.com>
2014-12-09 13:12:24 +0100

    Button renamed

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -5,9 +5,15 @@ using productionltd;
 namespace UnitTest {
     [TestClass]
     public class UnitTest1 {
+
         [TestMethod]
-        public void TestMethod1() {
+        public void ProcessTimeTest() {
+            Product p = new Product("Test Rist", true, "30");
+            OrderItem OI = new OrderItem(amount, p);
+            OI.Save();
+            int amount = 50;
             
+             
         }
     }
 }
